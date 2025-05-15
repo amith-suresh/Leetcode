480d920fb330a09be392cc48a5917956ff016107
Amith <amithsuresh1812@gmail.com>
2025-05-15 15:46:09 +0530

    Time: 53 ms (41.9%), Space: 55.7 MB (34.61%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -4,8 +4,7 @@
  */
 var reverse = function(x) {
     let str = x.toString().split('').reverse().join('');
-    if(x==1534236469
-||x===2147483647||x===-2147483648||x===1563847412||x===-1563847412||x===1147483648||x===1137464807||x===1235466808||x===1221567417){
+    if(parseInt(str)>2147483647 ){
         return 0
     }
     return x<0?-parseInt(str):parseInt(str)
